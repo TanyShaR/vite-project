c103d1dfdfde5bdb6281f126cbab7ced0947eed7
TanjaRetter <88773218+TanyShaR@users.noreply.github.com>
2025-07-04 09:59:03 +0200

    new

---
+++ src/components/ThemeSwitch/ThemeSwitch.jsx
@@ -0,0 +1,9 @@
+import "./ThemeSwitch.jsx";
+export const ThemeSwitch = () => {
+    return (
+        <div className="theme-switch">
+            <button>Dark</button>
+            <button>Light</button>
+        </div>
+    );
+};
