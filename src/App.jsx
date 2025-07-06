a79b1aa0a8efde79330d09471c83e61a1095c829
TanjaRetter <88773218+TanyShaR@users.noreply.github.com>
2025-07-06 15:27:55 +0200

    theme switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,30 @@
-import './App.css'
-import { About } from './components/About';
-import { Job } from './components/Job';
-import { TaskList } from './components/TaskList';
-import { ThemeSwitch } from './components/ThemeSwitch/ThemeSwitch';
+import "./App.css";
+import { About } from "./components/About";
+import { Job } from "./components/Job";
+import { TaskList } from "./components/TaskList";
+import { ThemeSwitch } from "./components/ThemeSwitch/ThemeSwitch";
 
 const tasks = [
-    {
-        id: 1,
-        title: "Farben",
-        deadline: '23:59',
-        completed: true,
-    },
-    {
-        id: 2,
-        deadline: '01-01-2026',
-        title: "Digital",
-    },
+  {
+    id: 1,
+    title: "Farben",
+    deadline: "23:59",
+    completed: true,
+  },
+  {
+    id: 2,
+    deadline: "01-01-2026",
+    title: "Digital",
+  },
 ];
 
 function App() {
+  const onSwitch = (theme) => {
+    console.log(theme);
+  };
   return (
     <>
-      <ThemeSwitch />
+      <ThemeSwitch onSwitch={onSwitch} />
       <About />
       <Job
         title="Kino Union Filmtheater. Berlin"
@@ -34,8 +37,8 @@ function App() {
         description="Motion Design und Animation, entwicklung interaktiver 2D-Hintergründe für Theaterstück "
       />
       <TaskList tasks={tasks} />
-    </>  
-  );      
+    </>
+  );
 }
 
 export default App;
--- src/components/ThemeSwitch/ThemeSwitch.jsx
+++ src/components/ThemeSwitch/ThemeSwitch.jsx
@@ -1,9 +1,43 @@
-import "./ThemeSwitch.jsx";
-export const ThemeSwitch = () => {
-    return (
-        <div className="theme-switch">
-            <button>Dark</button>
-            <button>Light</button>
-        </div>
-    );
+import { useState } from "react";
+import "./ThemeSwitch.css";
+export const ThemeSwitch = ({ onSwitch }) => {
+  const [theme, setTheme] = useState("light");
+  /* response[0]; // theme: light
+     response[1]; // setTheme() */
+  /* let theme = "light"; */
+  const onDarkClick = () => {
+    setTheme("dark");
+    /* theme = "dark";
+    onSwitch(theme); */
+    onSwitch("dark");
+  };
+  const onLightClick = () => {
+    setTheme("light");
+    /* theme = "light";
+    onSwitch(theme); */
+    onSwitch("light");
+  };
+  return (
+    <div className="theme-switch">
+      <div className="theme-switch__actions">
+        <button
+          onClick={onDarkClick}
+          className={`theme-swith__button ${
+            theme === "dark" ? "theme-switch__button_active" : ""
+          }`}
+        >
+          Dark
+        </button>
+        <button
+          onClick={onLightClick}
+          className={`theme-swith__button ${
+            theme === "light" ? "theme-switch__button_active" : ""
+          }`}
+        >
+          Light
+        </button>
+      </div>
+      <div className="theme-switch__value">{theme}</div>
+    </div>
+  );
 };
